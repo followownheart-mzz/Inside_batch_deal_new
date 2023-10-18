5ec8e439c671998677def6257fa4f070685047c1
followownheart-mzz <144082035+followownheart-mzz@users.noreply.github.com>
2023-10-18 09:25:55 +0800

    Update _22c396538b5647e1ab3a88afadab37d7.py

--- business/business_theme_deal/code_mode/deal/_22c396538b5647e1ab3a88afadab37d7.py
+++ business/business_theme_deal/code_mode/deal/_22c396538b5647e1ab3a88afadab37d7.py
@@ -16,7 +16,3 @@ class CodeMode:
         list = []
         list.append(self.data)
         return list
-# conn = pymongo.MongoClient(inputserver)['Inside-Data-Basic-Out']
-# collection=conn['10049a9724f84cddb8245b13a8a80fdf']
-# for data in collection.find({}):
-#     print(CodeMode(data, 'Inside-Data-Basic-Out','Inside-Data-Business-Out').process())
